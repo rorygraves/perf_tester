dabfcdb08ee8838e0cd43c0ca8c6685e297537e0
Mike Skells <mike.skells@talk21.com>
2017-10-16 19:30:18 +0100

    checkpoint

--- src/main/scala/org/perftester/ProfileMain.scala
+++ src/main/scala/org/perftester/ProfileMain.scala
@@ -46,8 +46,56 @@ object ProfileMain {
 //      TestConfig("00_2.12.2", BuildFromGit("21d12e9f5ec1ffe023f509848911476c1552d06f"),extraJVMArgs = List()),
 //      TestConfig("00_2.12.x", BuildFromGit("e1e8d050deb643ca56db1549e2e5a3114572a952"),extraJVMArgs = List())
 
-      TestConfig("00_backend-baseline", BuildFromDir("S:/scala/backend-before", false), extraArgs = List()),
-      TestConfig("00_backend", BuildFromDir("S:/scala/backend", false), extraArgs = List("-Yparallel-write=3"))
+//      TestConfig("00_backend-baseline", BuildFromDir("S:/scala/backend-before", false), extraArgs = List("-Yprofile-external-tool", "jvm")),
+      TestConfig("00_backend-baseline", BuildFromDir("S:/scala/backend-before", false), extraArgs = List("-Yprofile-run-gc", "*")),
+
+//      TestConfig("00_backend-0-mike", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "0")),
+//      TestConfig("00_backend-2-mike", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "2")),
+//      TestConfig("00_backend-4-mike", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "4")),
+//      TestConfig("00_backend-8-mike", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "8")),
+//      TestConfig("00_backend-12-mike", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "12")),
+//      TestConfig("00_backend-16-mike", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "16"))
+
+      TestConfig("00_backend-0", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "0", "-Yprofile-run-gc", "all")),
+      TestConfig("00_backend-1", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "1", "-Yprofile-run-gc", "all")),
+      TestConfig("00_backend-2", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "2", "-Yprofile-run-gc", "all")),
+      TestConfig("00_backend-3", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "3", "-Yprofile-run-gc", "all")),
+      TestConfig("00_backend-4", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "4", "-Yprofile-run-gc", "all"))
+//      TestConfig("00_backend-4-6", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "4", "-YmaxQueue", "6")),
+//      TestConfig("00_backend-4-8", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "4", "-YmaxQueue", "8")),
+//      TestConfig("00_backend-4-12", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "4", "-YmaxQueue", "12")),
+//      TestConfig("00_backend-4-16", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "4", "-YmaxQueue", "16")),
+//      TestConfig("00_backend-5", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "5", "-Yprofile-run-gc", "all")),
+//      TestConfig("00_backend-6", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "6", "-Yprofile-run-gc", "all")),
+//      TestConfig("00_backend-6-6", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "6", "-YmaxQueue", "6")),
+//      TestConfig("00_backend-6-8", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "6", "-YmaxQueue", "8")),
+//      TestConfig("00_backend-6-12", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "6", "-YmaxQueue", "12")),
+//      TestConfig("00_backend-6-16", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "6", "-YmaxQueue", "16")),
+//      TestConfig("00_backend-8", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "8", "-Yprofile-run-gc", "all"))
+//      TestConfig("00_backend-8-12", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "8", "-YmaxQueue", "12")),
+//      TestConfig("00_backend-8-16", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "8", "-YmaxQueue", "16")),
+//      TestConfig("00_backend-8-20", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "8", "-YmaxQueue", "20")),
+//      TestConfig("00_backend-8-24", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "8", "-YmaxQueue", "24")),
+//      TestConfig("00_backend-10", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "10")),
+//      TestConfig("00_backend-12", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "12")),
+//      TestConfig("00_backend-14", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "14")),
+//      TestConfig("00_backend-16", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "16"))
+
+
+//      TestConfig("00_backend-3-A", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "3", "-YmaxQueue", "16")),
+
+//      TestConfig("00_backend-0", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "0")),
+//      TestConfig("00_backend-1-4", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "1", "-YmaxQueue", "4")),
+//      TestConfig("00_backend-1-8", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "1", "-YmaxQueue", "8")),
+//      TestConfig("00_backend-1-X", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "1", "-YmaxQueue", "12")),
+//      TestConfig("00_backend-2-4", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "2", "-YmaxQueue", "4")),
+//      TestConfig("00_backend-2-8", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "2", "-YmaxQueue", "8")),
+//      TestConfig("00_backend-2-X", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "2", "-YmaxQueue", "12")),
+//      TestConfig("00_backend-3-4", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "3", "-YmaxQueue", "4")),
+//      TestConfig("00_backend-3-8", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "3", "-YmaxQueue", "8")),
+//      TestConfig("00_backend-3-X", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "3", "-YmaxQueue", "12")),
+//      TestConfig("00_backend-3-A", BuildFromDir("S:/scala/backend", false), extraArgs = List("-YmaxAdditionalWriterThreads", "3", "-YmaxQueue", "16"))
+
     )
 
     val results = commitsWithId map { testConfig =>
